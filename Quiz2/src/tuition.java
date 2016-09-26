6a9e95188d82fcfd8c6f29fc6398b28809fe505f
jennydefriece <jennydefriece@wifi-roaming-128-4-76-13.host.udel.edu>
2016-09-26 19:30:16 -0400

    Updated quiz 2

--- Quiz2/src/tuition.java
+++ Quiz2/src/tuition.java
@@ -3,13 +3,30 @@ import java.util.Scanner;
 public class tuition {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	System.out.print("Type initial tution cost:");
-	double tuition_cost=input.nextDouble();
-	
-	System.out.print("Type percentage increase per year:");
-	double percentage=input.nextDouble();
-	
-	for (i=0; i<=4)
+		System.out.print("Type initial tution cost:");
+		double initial_tuition = input.nextDouble();
+
+		System.out.print("Type percentage increase per year:");
+		double percentage = input.nextDouble();
 		
+		
+		
+		double total=0;
+		
+		for (int i=0; i <= 3; i++) {
+			double yearly_tuition=initial_tuition*percentage;  
+			double cost=total+=yearly_tuition;
 	
+		
+System.out.println("The total cost of tuition for this year will be:"+ cost);		
+			
+		}
 	}
+}
+
+			
+			
+	
+					
+					
+		
