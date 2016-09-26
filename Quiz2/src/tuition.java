8d210d29cddcdb10d02fba390f97152f929ca85f
jennydefriece <jennydefriece@wifi-roaming-128-4-76-13.host.udel.edu>
2016-09-26 15:20:36 -0400

    initial commit

---
+++ Quiz2/src/tuition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class tuition {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	System.out.print("Type initial tution cost:");
+	double tuition_cost=input.nextDouble();
+	
+	System.out.print("Type percentage increase per year:");
+	double percentage=input.nextDouble();
+	
+	for (i=0; i<=4)
+		
+	
+	}
