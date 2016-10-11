a8e40095ae1710c6165e445e78ddf3f0fc09bf1f
jennydefriece <jendef@udel.edu>
2016-10-11 01:35:24 -0400

    Update Triangle.java

--- Quiz2/src/Triangle.java
+++ Quiz2/src/Triangle.java
@@ -14,7 +14,8 @@ public Triangle(double side1, double side2, double side3){
 	this.side2=side2;
 	this.side3=side3;
 	
-}
+	if (!(side1 + side2 > side3) || !(side2>side1+side3) || (side1>side2+side3)){
+		throw new TriangleException();}
 public double accessSide1(){
 	return side1;
 	
